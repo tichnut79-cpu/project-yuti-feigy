d8e3f83ec6f463042893de4d71c19e67caba0dcc
feigy <tichnut84@8547900.org.il>
2026-04-21 14:21:59 +0300

    build of project

--- client/my-react-app/src/App.tsx
+++ client/my-react-app/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+    <h1>hello feigy!!!!!!!!!!!!!!!!!!!!</h1>
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
