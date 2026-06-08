9535026bd047a16f5ac4a73280275a0cac253ccb
feigy <tichnut84@8547900.org.il>
2026-06-08 17:39:08 +0300

    conection of client with server. complete

--- client/my-react-app/vite.config.ts
+++ client/my-react-app/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
+import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import babel from '@rollup/plugin-babel'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true,
-  }
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
 })
